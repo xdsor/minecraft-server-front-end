818fbc7e799e1c77066d043d597a2fba3fde427c
xdsor <xdsor2100@gmail.com>
2024-11-17 23:42:02 +0600

    Wrap online player card into link

--- src/pages/MainPage/components/PlayersOnlineComponent/PlayerOnlineComponent/PlayerOnlineComponent.jsx
+++ src/pages/MainPage/components/PlayersOnlineComponent/PlayerOnlineComponent/PlayerOnlineComponent.jsx
@@ -2,7 +2,7 @@ import "./PlayerOnlineComponent.css"
 import {Link} from "react-router-dom";
 
 export function PlayerOnlineComponent(props) {
-    const { name, details } = props.playerInfo;
+    const {name, details} = props.playerInfo;
     const health = details.health / 2;
     const foodLevel = details.food_level / 2;
 
@@ -35,24 +35,24 @@ export function PlayerOnlineComponent(props) {
     };
 
     return (
-        <div className="player-card d-flex align-items-center">
-            <i className="fas fa-user fa-2x me-3"></i>
-            <div className="player-details">
-                <div>
-                    <Link to={`/player/${name}`}>
+        <Link to={`/player/${name}`}>
+            <div className="player-card d-flex align-items-center">
+                <i className="fas fa-user fa-2x me-3"></i>
+                <div className="player-details">
+                    <div>
                         <strong>{name}</strong>
-                    </Link>
-                </div>
-                <div>
-                    Здоровье: <span className="health-icons">{renderHearts().join(' ')}</span>
-                </div>
-                <div>
-                    Сытость: <span className="hunger-icons">{renderHunger().join(' ')}</span>
-                </div>
-                <div>
-                    Уровень: <span>{details.experience}</span>
+                    </div>
+                    <div>
+                        Здоровье: <span className="health-icons">{renderHearts().join(' ')}</span>
+                    </div>
+                    <div>
+                        Сытость: <span className="hunger-icons">{renderHunger().join(' ')}</span>
+                    </div>
+                    <div>
+                        Уровень: <span>{details.experience}</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
